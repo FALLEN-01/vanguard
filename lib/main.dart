73bf2a6028063f2b00bab0cd18355be500e0f656
Jeffin Basil <jeffinbasil484@gmail.com>
2025-08-08 18:12:15 +0530

    added basic ui frame structure to folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,404 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ScramPad',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        brightness: Brightness.light,
+        colorScheme: const ColorScheme.light(
+          primary: Colors.blue,
+          surface: Color(0xFFFAFAFA),
+        ),
+        scaffoldBackgroundColor: const Color(0xFFF3F3F3),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ModernNotepadPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ModernNotepadPage extends StatefulWidget {
+  const ModernNotepadPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ModernNotepadPage> createState() => _ModernNotepadPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ModernNotepadPageState extends State<ModernNotepadPage> {
+  final TextEditingController _controller = TextEditingController();
+  bool _isBold = false;
+  bool _isItalic = false;
+  final String _currentHeading = 'Normal';
+  int _characterCount = 0;
+  int _lineNumber = 1;
+  int _columnNumber = 1;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_updateStats);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_updateStats);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _updateStats() {
+    final text = _controller.text;
+    final selection = _controller.selection;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _characterCount = text.length;
+      if (selection.isValid) {
+        final beforeCursor = text.substring(0, selection.baseOffset);
+        _lineNumber = '\n'.allMatches(beforeCursor).length + 1;
+        _columnNumber = beforeCursor.split('\n').last.length + 1;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      backgroundColor: const Color(0xFFF3F3F3),
+      body: Column(
+        children: [
+          // Custom Title Bar with Mica effect
+          Container(
+            height: 32,
+            decoration: BoxDecoration(
+              color: Colors.white.withValues(alpha: 0.7),
+              border: const Border(
+                bottom: BorderSide(color: Color(0xFFE0E0E0), width: 0.5),
+              ),
             ),
-          ],
+            child: Row(
+              children: [
+                const SizedBox(width: 12),
+                // Notepad icon
+                Container(
+                  width: 16,
+                  height: 16,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(2),
+                      bottomLeft: Radius.circular(2),
+                      bottomRight: Radius.circular(2),
+                    ),
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 2),
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(1),
+                        bottomLeft: Radius.circular(1),
+                      ),
+                    ),
+                    width: 3,
+                  ),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'Untitled - ScramPad',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black87,
+                  ),
+                ),
+                const Spacer(),
+                // Title bar controls
+                _buildTitleBarButton(Icons.palette, Colors.red),
+                _buildTitleBarButton(Icons.account_circle, Colors.orange),
+                _buildTitleBarButton(Icons.settings, Colors.grey),
+                _buildTitleBarButton(Icons.minimize, Colors.grey),
+                _buildTitleBarButton(Icons.crop_square, Colors.grey),
+                _buildTitleBarButton(Icons.close, Colors.grey),
+              ],
+            ),
+          ),
+
+          // Tab Bar
+          Container(
+            height: 36,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                bottom: BorderSide(color: Color(0xFFE0E0E0), width: 0.5),
+              ),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 4,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 6,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF0F0F0),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Untitled',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(width: 6),
+                      Icon(Icons.close, size: 12, color: Colors.grey),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 24,
+                  height: 24,
+                  margin: const EdgeInsets.only(left: 4),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: const Icon(Icons.add, size: 14, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+
+          // Toolbar
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.05),
+                  blurRadius: 4,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                _buildMenuButton('File'),
+                _buildMenuButton('Edit'),
+                _buildMenuButton('View'),
+                const SizedBox(width: 16),
+                _buildDropdown(_currentHeading, ['Normal', 'H1', 'H2', 'H3']),
+                const SizedBox(width: 8),
+                _buildDropdown('•', ['•', '1.', '→']),
+                const SizedBox(width: 16),
+                _buildFormatButton(Icons.format_bold, _isBold, () {
+                  setState(() => _isBold = !_isBold);
+                }),
+                _buildFormatButton(Icons.format_italic, _isItalic, () {
+                  setState(() => _isItalic = !_isItalic);
+                }),
+                _buildFormatButton(Icons.link, false, () {}),
+                const SizedBox(width: 8),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: const Text(
+                    'Aa',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Text Editor Area
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: TextField(
+                controller: _controller,
+                maxLines: null,
+                expands: true,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Segoe UI',
+                  color: Colors.black,
+                  height: 1.4,
+                  fontWeight: _isBold ? FontWeight.w600 : FontWeight.w400,
+                  fontStyle: _isItalic ? FontStyle.italic : FontStyle.normal,
+                ),
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(16),
+                  hintText: '',
+                ),
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.top,
+                cursorColor: Colors.black,
+                cursorWidth: 1.0,
+              ),
+            ),
+          ),
+
+          // Status Bar
+          Container(
+            height: 24,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: const BoxDecoration(
+              color: Color(0xFFF8F8F8),
+              border: Border(
+                top: BorderSide(color: Color(0xFFE0E0E0), width: 0.5),
+              ),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  'Ln $_lineNumber, Col $_columnNumber',
+                  style: const TextStyle(fontSize: 11, color: Colors.black54),
+                ),
+                const SizedBox(width: 16),
+                Text(
+                  '$_characterCount characters',
+                  style: const TextStyle(fontSize: 11, color: Colors.black54),
+                ),
+                const Spacer(),
+                const Text(
+                  'Plain text',
+                  style: TextStyle(fontSize: 11, color: Colors.black54),
+                ),
+                const SizedBox(width: 16),
+                const Text(
+                  '100%',
+                  style: TextStyle(fontSize: 11, color: Colors.black54),
+                ),
+                const SizedBox(width: 16),
+                const Text(
+                  'Windows (CRLF)',
+                  style: TextStyle(fontSize: 11, color: Colors.black54),
+                ),
+                const SizedBox(width: 16),
+                const Text(
+                  'UTF-8',
+                  style: TextStyle(fontSize: 11, color: Colors.black54),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitleBarButton(IconData icon, Color color) {
+    return Container(
+      width: 32,
+      height: 32,
+      margin: const EdgeInsets.only(right: 1),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {},
+          child: Icon(icon, size: 14, color: color.withValues(alpha: 0.8)),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildMenuButton(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(4),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 13,
+              color: Colors.black87,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDropdown(String value, List<String> items) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            value,
+            style: const TextStyle(fontSize: 12, color: Colors.black87),
+          ),
+          const SizedBox(width: 4),
+          Icon(Icons.arrow_drop_down, size: 16, color: Colors.grey.shade600),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFormatButton(IconData icon, bool isActive, VoidCallback onTap) {
+    return Container(
+      width: 28,
+      height: 28,
+      margin: const EdgeInsets.only(right: 4),
+      decoration: BoxDecoration(
+        color: isActive
+            ? Colors.blue.withValues(alpha: 0.1)
+            : Colors.transparent,
+        border: Border.all(
+          color: isActive
+              ? Colors.blue.withValues(alpha: 0.3)
+              : Colors.grey.shade300,
+        ),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(4),
+          child: Icon(
+            icon,
+            size: 16,
+            color: isActive ? Colors.blue : Colors.black54,
+          ),
+        ),
+      ),
     );
   }
 }
